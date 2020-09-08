9943ba62381a3aaaeb14da2f62fa7688ebcc58f4
keyboardWithDream <isharlan.hu@gmail.com>
2020-09-08 10:16:37 +0800

    使用Ajax传输Json对象

--- L2_Response/src/main/java/com/study/springmvc/controller/UserController.java
+++ L2_Response/src/main/java/com/study/springmvc/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author Harlan
