c2c4b10d24e21ed4f646c364c2c275a34d987620
keyboardWithDream <isharlan.hu@gmail.com>
2020-09-08 15:55:27 +0800

    MVC文件上传

--- L3_FileUpload/src/main/java/com/study/springmvc/controller/UserController.java
+++ L3_FileUpload/src/main/java/com/study/springmvc/controller/UserController.java
@@ -6,9 +6,11 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,8 +32,6 @@ public class UserController {
             file.mkdirs();
         }
 
-        System.out.println();
-
         //获取文件,获取上传文件项
         System.out.println("正在解析文件...");
         DiskFileItemFactory factory = new DiskFileItemFactory();
@@ -50,6 +50,25 @@ public class UserController {
             }
         }
         System.out.println("文件已保存...");
+        return "success";
+    }
+
+
+    @RequestMapping("/mvcUpload")
+    public String mvcUpload(HttpServletRequest request, MultipartFile file) throws IOException {
+        //设置保存路径
+        String path = request.getSession().getServletContext().getRealPath("/upload/");
+        File files = new File(path);
+        if (!files.exists()){
+            files.mkdirs();
+        }
+
+        //获取文件名
+        String name = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        //保存文件
+        file.transferTo(new File(path, uuid+"_"+name));
+
         return "success";
     }
 }
--- L3_FileUpload/src/main/webapp/index.jsp
+++ L3_FileUpload/src/main/webapp/index.jsp
@@ -11,8 +11,14 @@
     <title>文件上传</title>
 </head>
 <body>
-<a href="/user/fileUpload">测试文件上传</a>
+<h1>传统方式上传</h1>
 <form action="/user/fileUpload" enctype="multipart/form-data" method="post">
+    <label for="file">选择文件：</label><input type="file" name="file" id="file">
+    <input type="submit" value="上传">
+</form>
+<hr>
+<h1>SpringMVC方式上传</h1>
+<form action="/user/mvcUpload" enctype="multipart/form-data" method="post">
     <label for="file">选择文件：</label><input type="file" name="file" id="file">
     <input type="submit" value="上传">
 </form>
